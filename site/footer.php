792bac1c0687fe2d36ce6c1313a9b3527659fa25
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-05-05 20:33:24 -0300

    Base
    Começo do trabalho.

---
+++ site/footer.php
@@ -0,0 +1,35 @@
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+    </body>
+
+    <!--<footer style="position: absolute; bottom: 0px;">
+        <div class="rodape1">
+
+        </div>
+        <div>
+            <div>
+                <p>Atendimento</p>
+                <p>Política de Vendas, Trocas e Privacidade</p>
+                <p>Termos e Condições de Compra</p>
+                <p>Fale Conosco</p>
+            </div>
+            <div>
+                <p>Institucional</p>
+                <p>Sobre a Saraiva</p>
+                <p>Relações com Investidores</p>
+                <p>Nossas Lojas</p>
+                <p>Trabalhe Conosco</p>
+                <p>Seja um Parceiro</p>
+            </div>
+            <div>
+                <p>Formas de pagamento</p>
+                <p>Segurança</p>
+            </div>
+        </div>
+
+    </footer>-->
+
+    </html>
