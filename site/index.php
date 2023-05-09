1eb671449d0db5fa546f5588537905c1d30a1554
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-05-09 17:11:26 -0300

    Cabeçalho e rodapé
    Alteração no cabeçalho e rodapé

--- site/index.php
+++ site/index.php
@@ -3,201 +3,185 @@ include 'menutop.php';
 include 'header.php';
 ?>
 
-<div class="row">
-    <div>
-        <br>
-        <br>
+<div class="container" >
+    <div class="col">
+        <div class="row">
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">É Assim que Acaba</h4>
+                    <h1 style="font-size: 25px;">R$ 37,99</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
+                    <h1 style="font-size: 25px;">R$ 38,75</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
+                    <h1 style="font-size: 25px;">R$ 29,10</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
+                    <h1 style="font-size: 25px;">R$ 57,95</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">É Assim que Acaba</h4>
+                    <h1 style="font-size: 25px;">R$ 37,99</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
+                    <h1 style="font-size: 25px;">R$ 38,75</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
+                    <h1 style="font-size: 25px;">R$ 29,10</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
+                    <h1 style="font-size: 25px;">R$ 57,95</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">É Assim que Acaba</h4>
+                    <h1 style="font-size: 25px;">R$ 37,99</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
+                    <h1 style="font-size: 25px;">R$ 38,75</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
+                    <h1 style="font-size: 25px;">R$ 29,10</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
+                    <h1 style="font-size: 25px;">R$ 57,95</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">É Assim que Acaba</h4>
+                    <h1 style="font-size: 25px;">R$ 37,99</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
+                    <h1 style="font-size: 25px;">R$ 38,75</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
+                    <h1 style="font-size: 25px;">R$ 29,10</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+            <div class="flexbox">
+                <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
+                <div style="line-height: 10;">
+                    <p>
+                    <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
+                    <h1 style="font-size: 25px;">R$ 57,95</h1>
+                    </p>
+                </div>
+                <a href="#" class="btn botao">Comprar</button></a>
+            </div>
+        </div>
     </div>
-    <!--<div class="col-md-12 alert alert-dark">
-        <img src="https://lojasaraivanew.vtexassets.com/assets/vtex.file-manager-graphql/images/4a28650f-6021-4229-875b-8fac11fddb00___19a8ca437ca049ea84b2a637682393a9.png">
-        </div>-->
-
-    <div class="row">
-
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">É Assim que Acaba</h4>
-                <h1 style="font-size: 25px;">R$ 37,99</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
-                <h1 style="font-size: 25px;">R$ 38,75</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
-                <h1 style="font-size: 25px;">R$ 29,10</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
-                <h1 style="font-size: 25px;">R$ 57,95</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-
-
-    </div>
-
-    <div class="row">
-
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">É Assim que Acaba</h4>
-                <h1 style="font-size: 25px;">R$ 37,99</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
-                <h1 style="font-size: 25px;">R$ 38,75</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
-                <h1 style="font-size: 25px;">R$ 29,10</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
-                <h1 style="font-size: 25px;">R$ 57,95</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-
-
-    </div>
-
-    <div class="row">
-
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">É Assim que Acaba</h4>
-                <h1 style="font-size: 25px;">R$ 37,99</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
-                <h1 style="font-size: 25px;">R$ 38,75</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
-                <h1 style="font-size: 25px;">R$ 29,10</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
-                <h1 style="font-size: 25px;">R$ 57,95</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-
-
-    </div>
-
-    <div class="row">
-
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/424210/Livro-Assim-que-Acaba-Colleen-Hoover_1675436529_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">É Assim que Acaba</h4>
-                <h1 style="font-size: 25px;">R$ 37,99</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.kabum.com.br/produtos/fotos/magalu/423810/Livro-A-Culpa-Das-Estrelas_1675430875_gg.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
-                <h1 style="font-size: 25px;">R$ 38,75</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://images.tcdn.com.br/img/img_prod/597071/a_cinco_passos_de_voce_rachael_lippincott_227123_1_20200716135526.jpg" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
-                <h1 style="font-size: 25px;">R$ 29,10</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-        <div class="flexbox">
-            <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ02gP45xDi2IjWaJ_CANEbR2NEjtLcyXDuJm5FwUjzyE_jyEs1uGi9oWWL4fc57DGyFkEiE_dn7gW7mmkCZjsC4rsOygYWaq5ZcieK3qQgXZipSLC2O6Bb&usqp=CAE" class="imgvenda">
-            <div style="line-height: 10;">
-                <p>
-                <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
-                <h1 style="font-size: 25px;">R$ 57,95</h1>
-                </p>
-            </div>
-            <a href="#" class="btn botao">Comprar</button></a>
-        </div>
-
-
-    </div>
-
-
 </div>
+
 
 <?php
 include 'footer.php';
--- site/menutop.php
+++ site/menutop.php
@@ -1,43 +1,40 @@
-<nav class="navbar navbar-expand-lg" style="background-color: #F6CF80;">
+<nav class="navbar navbar-expand-lg barra">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="https://www.logogenio.pt/icons/preview/1798" width=276px height=552px class="img-fluid"></a>
+    <a class="navbar-brand" href="#"><img src="https://www.logogenio.pt/icons/preview/1798" width=180px height=380px class="img-fluid"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Página Inicial</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="hardware.php">Hardware</a>
+          <a class="nav-link" href="hardware.php">Perfil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="software.php">Software</a>
+          <a class="nav-link" href="software.php">Favoritos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="redes.php">Redes</a>
+          <a class="nav-link" href="redes.php">Carrinho</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Categorias
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="acao.php">Ação</a></li>
+            <li><a class="dropdown-item" href="comedia.php">Comédia</a></li>
+            <li><a class="dropdown-item" href="ficcao.php">Ficção</a></li>
+            <li><a class="dropdown-item" href="romance.php">Romance</a></li>
+            <li><a class="dropdown-item" href="terror.php">Terror</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="O que está buscando?" aria-label="Search">
+        <button class="btn btn-outline-success busca
+        " type="submit">Buscar</button>
       </form>
     </div>
   </div>
