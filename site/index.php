77fa4c029a9145a7676567358f58c5d456cd8fa5
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-05-13 20:31:30 -0300

    Edição nas Imagens
    Salvando, recortando e padronizando  as imagens da capa dos livros. Alterações básicas no CSS e também no HTML.

--- site/index.php
+++ site/index.php
@@ -5,167 +5,166 @@ include 'header.php';
 
 <div class="row">
     <div class="flexbox">
-        <img src="img/livros/e_assim_que_acaba.jpg" class="imgvenda">
+        <img src="img/livros/capa_e_assim_que_acaba-375x500.jpg" alt="Capa do livro É Assim que Acaba">
         <p>
         <h4 style="font-size: 15px;">É Assim que Acaba</h4>
         <h1 style="font-size: 25px;">R$ 37,99</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_culpa_e_das_estrelas.jpg" class="imgvenda">
+        <img src="img/livros/capa_a_culpa_e_das_estrelas-375x500.jpg" alt="Capa do livro A Culpa é Das Estrelas">
         <p>
         <h4 style="font-size: 15px;">A Culpa é Das Estrelas</h4>
         <h1 style="font-size: 25px;">R$ 38,75</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_cinco_passos_de_voce.jpg" class="imgvenda">
+        <img src="img/livros/capa_a_cinco_passos_de_voce-375x500.jpg" alt="Capa do livro A Cinco Passos de Você">
         <p>
         <h4 style="font-size: 15px;">A Cinco Passos de Você</h4>
         <h1 style="font-size: 25px;">R$ 29,10</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/orgulho_e_preconceito.jpg" class="imgvenda">
+        <img src="img/livros/capa_orgulho_e_preconceito-375x500.jpg" alt="Capa do livro Orgulho e Preconceito">
         <p>
         <h4 style="font-size: 15px;">Orgulho e Preconceito</h4>
         <h1 style="font-size: 25px;">R$ 57,95</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_divina_comedia.jpg" class="imgvenda">
+        <img src="img/livros/capa_a_divina_comedia_inferno-375x500.jpg" alt="Capa do livro A Divina Comédia">
         <p>
-        <h4 style="font-size: 15px;">A Divina Comédia</h4>
+        <h4 style="font-size: 15px;">A Divina Comédia Inferno</h4>
         <h1 style="font-size: 25px;">R$ 46,41</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/cade_voce_bernadete.jpg" class="imgvenda">
+        <img src="img/livros/capa_cade_voce_bernadette-375x500.jpg" alt="Capa do livro Cade Você Bernadette?">
         <p>
         <h4 style="font-size: 15px;">Cade Você Barnadette?</h4>
         <h1 style="font-size: 25px;">R$ 34,50</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/meu_coracao_de_pedra_pomes.jpg" class="imgvenda">
+        <img src="img/livros/capa_meu_coracao_de_pedra_pomes-375x500.jpg" alt="Capa do livro Meu Coração de Pedra-Pomes">
         <p>
-        <h4 style="font-size: 15px;">Meu Coração de pedra-pomes</h4>
+        <h4 style="font-size: 15px;">Meu Coração de Pedra-Pomes</h4>
         <h1 style="font-size: 25px;">R$ 25,45</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/galvez_o_imperador_do_acre.jpg" class="imgvenda">
+        <img src="img/livros/capa_galvez_imperador_do_acre-375x500.jpg" alt="Capa do livro Galvez, Imperador do Acre">
         <p>
         <h4 style="font-size: 15px;">Galvez, Imperador do Acre</h4>
         <h1 style="font-size: 25px;">R$ 26,99</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/o_hobbit.jpg" class="imgvenda">
+        <img src="img/livros/capa_o_hobbit-375x500.jpg" alt="Capa do livro O Hobbit">
         <p>
         <h4 style="font-size: 15px;">O Hobbit</h4>
         <h1 style="font-size: 25px;">R$ 45,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/trono_de_vidro.jpg" class="imgvenda">
+        <img src="img/livros/capa_trono_de_vidro-375x500.jpg" alt="Capa do livro Trono de Vidro">
         <p>
         <h4 style="font-size: 15px;">Trono de Vidro</h4>
         <h1 style="font-size: 25px;">R$ 35,10</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_rainha_vermelha.jpeg" class="imgvenda">
+        <img src="img/livros/capa_a_rainha_vermelha-375x500.jpg" alt="Capa do livro A Rainha Vermelha">
         <p>
         <h4 style="font-size: 15px;">A Rainha Vermelha</h4>
         <h1 style="font-size: 25px;">R$ 34,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_maquina_do_tempo.jpg" class="imgvenda">
+        <img src="img/livros/capa_a_maquina_do_tempo-375x500.jpg" alt="Capa do livro A Máquna Do Tempo">
         <p>
         <h4 style="font-size: 15px;">A Máquina do Tempo</h4>
         <h1 style="font-size: 25px;">R$ 35,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/dracula.jpg" class="imgvenda">
+        <img src="img/livros/capa_dracula-375x500.jpg" alt="Capa do livro Dracula">
         <p>
         <h4 style="font-size: 15px;">Dracula</h4>
         <h1 style="font-size: 25px;">R$ 89,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/o_vilarejo.jpg" class="imgvenda">
+        <img src="img/livros/capa_o_vilarejo-375x500.jpg" alt="Capa do livro O Vilarejo">
         <p>
         <h4 style="font-size: 15px;">O Vilarejo</h4>
         <h1 style="font-size: 25px;">R$ 31,94</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/coraline.jpg" class="imgvenda">
+        <img src="img/livros/capa_coraline-375x500.jpg" alt="Capa do livro Coraline">
         <p>
         <h4 style="font-size: 15px;">Coraline</h4>
         <h1 style="font-size: 25px;">R$ 52,00</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/o_exorcista.jpg" class="imgvenda">
+        <img src="img/livros/capa_o_exorcista-375x500.jpg" alt="Capa do livro O Exorcista">
         <p>
         <h4 style="font-size: 15px;">O Exorcista</h4>
         <h1 style="font-size: 25px;">R$ 59,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/duna.jpg" class="imgvenda">
+        <img src="img/livros/capa_duna-375x500.jpg" alt="Capa do livro Duna">
         <p>
         <h4 style="font-size: 15px;">Duna</h4>
         <h1 style="font-size: 25px;">R$ 81,24</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/eu_robo.jpg" class="imgvenda">
+        <img src="img/livros/capa_eu_robo-375x500.jpg" alt="Capa do livro Eu, Robô">
         <p>
         <h4 style="font-size: 15px;">Eu, Robô</h4>
         <h1 style="font-size: 25px;">R$ 38,75</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/neuromancer.jpg" class="imgvenda">
+        <img src="img/livros/capa_neuromancer-375x500.jpg" alt="Capa do livro Neuromancer">
         <p>
         <h4 style="font-size: 15px;">Neuromancer</h4>
         <h1 style="font-size: 25px;">R$ 59,90</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
     <div class="flexbox">
-        <img src="img/livros/a_biblioteca_da_meia_noite.jpg" class="imgvenda">
+        <img src="img/livros/capa_a_biblioteca_da_meia_noite-375x500.jpg" alt="Capa do livro A Biblioteca da Meia Noite">
         <p>
         <h4 style="font-size: 15px;">A Biblioteca da Meia Noite</h4>
         <h1 style="font-size: 25px;">R$ 38,52</h1>
         </p>
-        <a href="#" class="btn botao">Comprar</button></a>
+        <button href="#" class="btn botao">Comprar</button>
     </div>
 </div>
-
 
 <?php
 include 'footer.php';
